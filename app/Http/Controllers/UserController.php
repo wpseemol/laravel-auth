5fc31d2447a0971f3da0d761d51b4538ff9b659c
wpseemol <wpseemol@gmail.com>
2025-01-08 17:10:19 +0600

    make user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+
+    function UserRegistration(Request $request)
+    {
+        return null;
+    }
+
+
+}
