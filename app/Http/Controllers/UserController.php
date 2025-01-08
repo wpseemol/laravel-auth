6b55580d33b2bc8728b70ad64fbaf212eacc179d
wpseemol <wpseemol@gmail.com>
2025-01-08 17:41:39 +0600

    create user-register route for register user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
+
+
+/**
+ * Summary of UserController
+ * User Controller has method to created or register user
+ * `UserRegistration` method
+ */
 class UserController extends Controller
 {
     //
 
-    function UserRegistration(Request $request)
+    /**
+     * User Registration method Write here
+     * @param \Illuminate\Http\Request $request
+     * @return User
+     * Return User instance here.
+     */
+    function UserRegistration(Request $request): User|null
     {
-        return null;
+
+
+        $firstName = $request->input("firstName");
+        $lastName = $request->input("lastName");
+        $email = $request->input("email");
+        $mobile = $request->input("mobile");
+        $password = $request->input("password");
+
+
+        $result = User::create([
+            "firstName" => $firstName,
+            "lastName" => $lastName,
+            "email" => $email,
+            "mobile" => $mobile,
+            "password" => $password
+
+        ]);
+
+        return $result;
     }
 
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ class User extends Model
     protected $fillable = ["firstName", "lastName", "email", "mobile", "password", "otp"];
 
     protected $attributes = [
-        "otp" => "0"
+        "otp" => ""
     ];
 
 
